ef69d5577703cd0ad49f7a39ba178176b1293dec
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-05-05 22:31:58 +0200

    Fixed: alamcenamiento en local y cumplimentar edad

--- js/main.js
+++ js/main.js
@@ -8,24 +8,23 @@ function validarFormulario(){
     //definimos dos espacios en memoria para enlazarse en el DOM
     const nombre= document.getElementById("nombre").value;
     const apellido= document.getElementById("apellido").value;
+    const edad= document.getElementById("edad").value;
     //condicional para el envio del formulario
     if (nombre === "" || apellido === "" || edad === ""){
         alert("Dato requerido");
         //se evita que el formulario se envíe
         return false;
     };
-    
-     console.log("Nombre y apellido bien");
-     const edad= document.getElementById("edad").value;
-     if (edad == ""){
+    console.log("Nombre y apellido bien");
+        if (edad === ""){
         alert("No has introducido tu edad");
         return false
-     } else if(edad < 18){
+    } else if(edad < 18){
         alert("A tu casa niño a ver los lunis");
         return false
-     } else{
+    } else{
         alert("Cuentale al tito que te ha pasado");
-     }
+    }
      //indicamos que almacene los datos en localStorage
      localStorage.setItem("nombreDenunciante", nombre);
      localStorage.setItem("apellidoDenunciante", apellido);
@@ -34,7 +33,7 @@ function validarFormulario(){
      return true;
 };
 
-windows.onload = function(){
+window.onload = function(){
     const nombreGuardado = localStorage.getItem("nombreDenunciante");
     const apellidoGuardado = localStorage.getItem("apellidoDenunciante");
     const edadGuardado = localStorage.getItem("edadDenunciante");
