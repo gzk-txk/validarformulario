a6b5c3129d096398fe518d61280e0c74715e9989
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-05-05 21:18:51 +0200

    Formulario validado a falta de alguno campos por validar

---
+++ js/main.js
@@ -0,0 +1,50 @@
+//VARIABLES
+
+//EVENTOS
+
+//FUNCIONES
+
+function validarFormulario(){
+    //definimos dos espacios en memoria para enlazarse en el DOM
+    const nombre= document.getElementById("nombre").value;
+    const apellido= document.getElementById("apellido").value;
+    //condicional para el envio del formulario
+    if (nombre === "" || apellido === "" || edad === ""){
+        alert("Dato requerido");
+        //se evita que el formulario se envíe
+        return false;
+    };
+    
+     console.log("Nombre y apellido bien");
+     const edad= document.getElementById("edad").value;
+     if (edad == ""){
+        alert("No has introducido tu edad");
+        return false
+     } else if(edad < 18){
+        alert("A tu casa niño a ver los lunis");
+        return false
+     } else{
+        alert("Cuentale al tito que te ha pasado");
+     }
+     //indicamos que almacene los datos en localStorage
+     localStorage.setItem("nombreDenunciante", nombre);
+     localStorage.setItem("apellidoDenunciante", apellido);
+     localStorage.setItem("edadDenunciante", edad);
+     //finaliza la funcion
+     return true;
+};
+
+windows.onload = function(){
+    const nombreGuardado = localStorage.getItem("nombreDenunciante");
+    const apellidoGuardado = localStorage.getItem("apellidoDenunciante");
+    const edadGuardado = localStorage.getItem("edadDenunciante");
+    if (nombreGuardado){
+        document.getElementById("nombre").value = nombreGuardado;
+    }
+    if (apellidoGuardado){
+        document.getElementById("apellido").value = apellidoGuardado;
+    }
+    if (edadGuardado){
+        document.getElementById("edad").value = edadGuardado;
+    }
+}
